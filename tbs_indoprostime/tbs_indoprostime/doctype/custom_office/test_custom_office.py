61da8a510e278e17b9c24964e683f8dd349e8a60
rickysut <ricky.sutanto@gmail.com>
2026-09-12 20:50:15 +0700

    file file

---
+++ tbs_indoprostime/tbs_indoprostime/doctype/custom_office/test_custom_office.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Team ERP and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCustomOffice(FrappeTestCase):
+	pass
