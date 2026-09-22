a169bf5bee0b079fb3293df864fe30a16913592c
rickysut <ricky.sutanto@gmail.com>
2026-09-22 21:57:03 +0700

    notify party

--- tbs_indoprostime/tbs_indoprostime/doctype/notify_party/notify_party.py
+++ tbs_indoprostime/tbs_indoprostime/doctype/notify_party/notify_party.py
@@ -1,9 +1,25 @@
-# Copyright (c) 2026, Team ERP and contributors
-# For license information, please see license.txt
-
-# import frappe
+# tbs_indoprostime/tbs_indoprostime/doctype/notify_party/notify_party.py
+import frappe
 from frappe.model.document import Document
 
-
 class NotifyParty(Document):
-	pass
+    def validate(self):
+        if self.port and self.country:
+            port_country = frappe.db.get_value('Port', self.port, 'country')
+            if port_country != self.country:
+                frappe.throw(f"Port {self.port} tidak sesuai dengan Country {self.country}")
+
+
+@frappe.whitelist()
+def get_countries_with_port(doctype, txt, searchfield, start, page_len, filters):
+    return frappe.db.sql("""
+        SELECT DISTINCT country, country
+        FROM `tabPort`
+        WHERE country LIKE %(txt)s
+        ORDER BY country
+        LIMIT %(start)s, %(page_len)s
+    """, {
+        'txt': f'%{txt}%',
+        'start': start,
+        'page_len': page_len
+    })
